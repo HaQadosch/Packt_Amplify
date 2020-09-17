c7bbfee95554002344c2a19c58e0aecf3f85f4a7
haqadosch <haqadosch@pm.me>
2020-09-17 22:34:28 +0100

    react: create basic layout for Notes

--- notetaker/src/App.tsx
+++ notetaker/src/App.tsx
@@ -5,10 +5,10 @@ import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 export const App: React.FC = () => {
   const [authState, setAuthState] = useState<AuthState>()
   const [user, setUser] = useState<object | undefined>()
+  const [notes/*, setNotes*/] = useState([{ id: 1, text: "hello world" }])
 
   useEffect(() => {
     return onAuthUIStateChange((nextAuthState, authData) => {
-      console.log({ nextAuthState, authData })
       setAuthState(nextAuthState)
       setUser(authData)
     })
@@ -16,10 +16,39 @@ export const App: React.FC = () => {
 
   return authState === AuthState.SignedIn && user
     ? (
-      <main>
-        <AmplifySignOut />
-        Hello {(user as object & { username: string } | undefined)?.username}
-      </main>
+      <>
+        <header>
+          <p>
+            Hello {(user as object & { username: string } | undefined)?.username}
+          </p>
+          <h1>Note Taker</h1>
+        </header>
+        <main>
+          <article>
+            <form>
+              <fieldset>
+                <legend>How do we feel today?</legend>
+                <label htmlFor="inputNote"></label>
+                <input id="inputNote" type="text" placeholder="Write down something" />
+                <button type="submit">Add Note</button>
+              </fieldset>
+            </form>
+          </article>
+          <article>
+            <ul>
+              {notes.map(({ id, text }) => (
+                <li key={id}>
+                  <p>{text}</p>
+                  <button><span role="img" aria-label="deleteNote">✖️</span></button>
+                </li>
+              ))}
+            </ul>
+          </article>
+        </main>
+        <footer>
+          <AmplifySignOut />
+        </footer>
+      </>
     )
     : <AmplifyAuthenticator />
 }
